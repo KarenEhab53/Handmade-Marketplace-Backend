87b6d40217e120eecd72a1a0e966804417f11c4d
KarenEhab53 <karenehab53@gmail.com>
2026-02-28 21:28:47 +0200

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-
+const jwt=require("jsonwebtoken")
 const addUser = async (req, res) => {
   try {
     const { name, email, password, phone = [], address, role } = req.body;
@@ -36,7 +36,45 @@ const addUser = async (req, res) => {
     res.status(500).json({ msg: "Server Error", error: err.message });
   }
 };
-
+const login = async (req, res) => {
+  try {
+    //validate req.body
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(400).json({ msg: "Missing Data" });
+    //check user exist                   password is invisible by default in every query
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) return res.status(404).json({ msg: "User not found" });
+    //check password hashed
+    const decodepass = await bcrypt.compare(password, user.password);
+    if (!decodepass) return res.json({ msg: "wrong password " });
+    //generate jwt
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      },
+    );
+    res.status(200).json({
+      success: true,
+      msg: "user login successfully",
+      data: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        address: user.address,
+        phone: user.phone,
+        profileImage: user.profileImage,
+      },
+      token: token,
+    });
+  } catch (err) {
+    console.log(err); // ✅ this will show the real error in terminal
+    res.status(500).json({ msg: "Server Error", error: err.message });
+  }
+};
 module.exports = {
   addUser,
+  login
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { addUser } = require("../controllers/userController");
+const { addUser, login } = require("../controllers/userController");
 const upload = require("../middleware/uploadMiddleware");
-
 router.post("/register", upload.single("profileImage"), addUser);
-
+router.post("/login", login);
 module.exports = router;
