7ef4c410118a52497594e2c5ed10a68f67aa3372
KarenEhab53 <karenehab53@gmail.com>
2026-03-16 10:38:27 +0200

    cors

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend origin
+    credentials: true,
+  }),
+);
 async function dbconnection() {
   try {
     await mongoose.connect(
